2f378ad338bb736b196049023a7b3262bfde1883
mathe <matheovll35@gmail.com>
2024-11-03 22:29:34 +0100

    [DEV] fin de l'api user

--- user/user.py
+++ user/user.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from operator import contains
+from urllib.request import localhost
+
 from flask import Flask, render_template, request, jsonify, make_response
 import requests
 import json
@@ -8,9 +12,8 @@ app = Flask(__name__)
 PORT = 3203
 HOST = '0.0.0.0'
 
-booking_port = 3201
-movie_port =
-showtime_port =
+booking_ws = "http://localhost:3201"
+movie_ws = "http://localhost:3200"
 
 # Load the users from the json file
 def load_users():
@@ -22,11 +25,86 @@ def save_users(users_list):
    with open('{}/databases/users.json'.format("."), 'w') as jsf:
       json.dump({"movies": users_list}, jsf, indent=4)
 
+def user_exist(userid):
+   exist = False
+   for user in users:
+      if user["id"] == userid:
+         exist = True
+   return exist
+
 users = load_users()
 
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+
+@app.route("/users/<string:userid>/movies/watched-count", methods=["GET"])
+def get_user_watchedcount(userid):
+   """Return the number of movie booking booking the today date"""
+
+   today = datetime.today().strftime('%Y%m%d')
+   counter = 0
+
+   # check to be sure the user is in the database
+   if user_exist(userid):
+
+      # request to get the user bookings
+      response = requests.get(booking_ws + "/bookings/" + userid)
+
+      # if the request has no problems and the dates list exist in json response
+      if response.status_code == 200 and response.json()["dates"]:
+
+         # iterate over all the booking dates
+         for date in response.json()["dates"]:
+
+            # if the date is before the today date
+            if date["date"] < today:
+
+               # we iterate over the movies of the date and add 1 each times
+               for movie in date["movies"]:
+                  counter += 1
+
+      return make_response(jsonify({"watched-count": counter}),200)
+   return make_response("User not found", 404)
+
+@app.route("/users/<string:userid>/movies/titles", methods=["GET"])
+def get_booked_movie_titles(userid):
+   """Return all the title of the movies in user booking"""
+
+   titles = []
+
+   # check to be sure the user is in the database
+   if user_exist(userid):
+
+      # request to get the user bookings
+      response_booking = requests.get(booking_ws + "/bookings/" + userid)
+
+      # if the request has no problems and the dates list exist in json response
+      if response_booking.status_code == 200 and response_booking.json()["dates"]:
+
+         # iterate over all the booking dates
+         for date in response_booking.json()["dates"]:
+
+            # we iterate over the movies of the date and add 1 each times
+            for movie in date["movies"]:
+
+               # get the movie object
+               response_movie = requests.get(movie_ws + "/movies/" + movie)
+
+               # check if we get response
+               if response_movie.status_code == 200 and response_movie.json()["title"]:
+
+                  # check if the title is not already in the movie list
+                  if not response_movie.json()["title"] in titles:
+
+                     # add title to the list
+                     titles.append(response_movie.json()["title"])
+
+         return make_response(jsonify({"titles": titles}), 200)
+   return make_response("User ID not found")
+
+
+
 
 
 if __name__ == "__main__":
